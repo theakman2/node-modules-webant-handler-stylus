1fd17cc212cd5caba483dec5a5d01a9bf50e79b9
theakman2 <theakman2@hotmail.com>
2013-11-09 13:19:31 +0000

    API changes to accommodate webant v0.8.0.

--- README.md
+++ README.md
@@ -12,47 +12,24 @@ If for some reason you'd like to use the module outside of webant, install as fo
 
 ## Usage
 
-Ensure the `stylus` handler is present in your webant configuration file.
-
-An example configuration file which uses this handler may look like this:
+Ensure the `stylus` handler is present in your webant configuration file. For example:
 
 ````json
 {
-    "jsEntryPath":"%%base%%/src/js/main.js",
-    "jsDestPath":"%%base%%/build/main.js",
+    "entry":"src/js/main.js",
+    "dest":"build/main.js",
     "handlers":{
-        "stylus":{}
+        "stylus":{
+            "compress":true
+        }
     }
 }
 ````
 
-You may now `require` CSS files via the function style or the comment style:
-
-````javascript
-//=>require ../path/to/styles.stylus
-````
+You may now `require` stylus files:
 
 ````javascript
 require("../path/to/styles.stylus");
-````
-
-If a stylus file is included via the function style, the compiled CSS will be inserted into the DOM at runtime.
-
-If on the other hand the stylus file is included via the comment style then the `require`d compiled CSS will be added to the file at `cssDestPath` and will be added to the `{{internalCss}}` variable which can be used within the handlebars template file at `htmlEntryPath`.
-
-Both `cssDestPath` and `htmlEntryPath` are optional settings that may be added to the webant configuration file. E.g:
-
-````json
-{
-    "jsEntryPath":"%%base%%/src/js/main.js",
-    "jsDestPath":"%%base%%/build/main.js",
-    "cssDestPath":"%%base%%/build/styles/css",
-    "htmlEntryPath":"%%base%%/src/index.hbs",
-    "htmlDestPath":"%%base%%/build/index.html",
-    "handlers":{
-        "stylus":{}
-    }
-}
 ````
 
 See the [webant](https://github.com/theakman2/node-modules-webant) module for more information.
--- test/index.js
+++ test/index.js
@@ -1,92 +1,59 @@
-var Handler = require("../lib/index.js");
-var HandlerBase = require("./lib/Handler.js");
+var fs = require("fs");
+var path = require("path");
 
-function createHandler(Handler,settings) {
-	var handlerBase = new HandlerBase(settings);
-	
-	Handler.prototype = handlerBase;
-	Handler.prototype.constructor = Handler;
-	
-	return new Handler();
-}
+var Handler = require("../lib/index.js");
 
 var tests = {
-	"test stylus 1":function(assert,done) {
-		var handler = createHandler(Handler);
-		var data = {
-			filePath:"http://sfi9s.sdf.sd/vk93k.handlebars?bla=3",
-			raw:"http://sfi9s.sdf.sd/vk93k.handlebars?bla=3",
-			requireType:"comment"
-		};
-		handler.willHandle(data,function(err,yes){
-			assert.strictEqual(err,null,"Handler should not report any errors.");
-			assert.strictEqual(yes,false,"Handler should not claim to handle this file.");
-			done();
-		});
+	"test filetypes":function(assert) {
+		var handler = new Handler();
+		var data = [
+		            "https://mysite.co.uk/bla.js",
+		            "//cdn.google.com/path/to/assets.css",
+		            "path/to/assets.styl",
+		            "/abs/path/to/assets.styl",
+		            "path/to/assets.stylus",
+		            "/abs/path/to/assets.stylus",
+		            "@@hbs/runtime",
+		            "@@css/addStylesheet"
+		            ];
+		assert.deepEqual(
+			data.map(handler.willHandle),
+			[false,false,true,true,true,true,false,true],
+			"Should handle the correct files."
+		);
 	},
-	"test stylus 2":function(assert,done) {
-		var handler = createHandler(Handler);
-		var data = {
-			filePath:__dirname+"/path/to/bad/file.css",
-			raw:"file.css",
-			requireType:"function"
-		};
-		handler.willHandle(data,function(err,yes){
-			assert.strictEqual(err,null,"Handler should not report any errors.");
-			assert.strictEqual(yes,false,"Handler should not claim to handle this file.");
-			done();
-		});
-	},
-	"test stylus correct file type":function(assert,done) {		
-		var handler = createHandler(Handler);
-		var data = {
-			filePath:__dirname+"/styles.styl",
-			requireType:"comment"
-		};
-		handler.handle(data,function(resp){
-			assert.deepEqual(
-				resp,
-				{
-					type:"internalCss",
-					content:".foo .bar {\n  color: #ecd;\n}\n"
-				},
-				"Handler should call 'update' correctly."
+	"test content 1":function(assert,done) {		
+		var handler = new Handler();
+		handler.handle(__dirname+"/styles.styl",function(err,content){
+			assert.ok(!err,"There should be no errors handling this filetype.");
+			assert.equal(
+				content,
+				'require("@@css/addStylesheet",function(add){ add(".foo .bar {\\n  color: #ecd;\\n}\\n"); });',
+				"Handler should return the right content."
 			);
 			done();
 		});
 	},
-	"test stylus correct file type with compression (2)":function(assert,done) {		
-		var handler = createHandler(Handler,{compress:true});
-		var data = {
-			filePath:__dirname+"/styles.stylus",
-			requireType:"comment"
-		};
-		handler.handle(data,function(resp){
-			assert.deepEqual(
-				resp,
-				{
-					type:"internalCss",
-					content:".foo .bar{color:#ecd}\n"
-				},
-				"Handler should call 'update' correctly."
+	"test content 2":function(assert,done) {
+		var handler = new Handler({compress:true});
+		handler.handle(__dirname+"/styles.stylus",function(err,content){
+			assert.ok(!err,"There should be no errors handling this filetype.");
+			assert.equal(
+				content,
+				'require("@@css/addStylesheet",function(add){ add(".foo .bar{color:#ecd}\\n"); });',
+				"Handler should return the right content."
 			);
 			done();
 		});
 	},
-	"test stylus correct file type with compression and as javascript":function(assert,done) {		
-		var handler = createHandler(Handler,{compress:true});
-		var data = {
-			filePath:__dirname+"/styles.styl",
-			requireType:"function"
-		};
-		handler.handle(data,function(resp){
-			assert.deepEqual(
-				resp,
-				{
-					type:"internalJs",
-					content:'require("@@css/addStylesheet")(".foo .bar{color:#ecd}");'
-				},
-				"Handler should call 'update' correctly."
+	"test content 3":function(assert,done) {		
+		var handler = new Handler({compress:true});
+		handler.handle("@@css/addStylesheet",function(err,content){
+			assert.ok(!err,"There should be no errors handling this filetype.");
+			assert.equal(
+				content,
+				fs.readFileSync(path.join(__dirname,"..","lib","data","addStylesheet.js")).toString(),
+				"Handler should update with correct content."
 			);
 			done();
 		});
